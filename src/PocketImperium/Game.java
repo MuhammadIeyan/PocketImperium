2cc0b8472899dc4d0abdff5bc718aef6b7f0b479
Benj221 <benjamin.carre22@gmail.com>
2025-01-09 10:35:36 +0100

    removed import

--- src/PocketImperium/Game.java
+++ src/PocketImperium/Game.java
@@ -7,11 +7,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
-
-import PocketGame.Hex;
-import PocketGame.Player;
-import PocketGame.Sector;
-
 public class Game implements Serializable{
 	private List<Player> playerList;
 	private int turnNumber;
