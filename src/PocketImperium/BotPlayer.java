b0644ad6b8e97b0c0f6aaa9c964b86abe20353ca
Benj221 <benjamin.carre22@gmail.com>
2025-01-09 10:35:01 +0100

    fixed expand for bot

--- src/PocketImperium/BotPlayer.java
+++ src/PocketImperium/BotPlayer.java
@@ -2,7 +2,9 @@ package PocketImperium;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class BotPlayer extends Player implements Serializable {
@@ -61,35 +63,42 @@ public class BotPlayer extends Player implements Serializable {
 
     @Override
     public void expand(int shipNumber) {
-        // Sélectionner un secteur que le bot possède
+        // Récupérer les secteurs possédés par le bot
         List<Sector> ownedSectors = getOwnedSector();
         if (ownedSectors.isEmpty()) {
             System.out.println(getName() + " has no sectors to expand.");
             return;
         }
 
-        // Choisir un secteur aléatoirement parmi ceux possédés
-        Sector selectedSector = ownedSectors.get(new Random().nextInt(ownedSectors.size()));
-
-        // Trouver les hexagones disponibles dans ce secteur
-        List<Hex> availableHexes = new ArrayList<>();
-        for (int i = 0; i < selectedSector.getSection().size(); i++) {
-            Hex hex = selectedSector.getSection().get(i);
-            if (hex.fleetAvailablity() > 0) { // Si l'hexagone a de la place
-                availableHexes.add(hex);
+        // Trouver les hexagones possédés dans les secteurs contrôlés
+        Map<Sector, List<Hex>> ownedHexesInSectors = new HashMap<>();
+        for (Sector sector : ownedSectors) {
+            List<Hex> ownedHexes = new ArrayList<>();
+            for (Hex hex : sector.getSection()) {
+                if (hex.getOwner() == this) {
+                    ownedHexes.add(hex); // Ajouter uniquement les hexagones possédés par le bot avec de la place
+                }
+            }
+            if (!ownedHexes.isEmpty()) {
+                ownedHexesInSectors.put(sector, ownedHexes);
             }
         }
 
-        if (availableHexes.isEmpty()) {
-            System.out.println(getName() + " has no available hexes to expand in sector " + selectedSector.getSectorId());
+        if (ownedHexesInSectors.isEmpty()) {
+            System.out.println(getName() + " has no owned hexes with available space to expand.");
             return;
         }
 
-        // Choisir un hexagone parmi ceux disponibles
+        // Choisir un secteur parmi ceux avec des hexagones possédés disponibles
+        List<Sector> availableSectors = new ArrayList<>(ownedHexesInSectors.keySet());
+        Sector selectedSector = availableSectors.get(new Random().nextInt(availableSectors.size()));
+
+        // Choisir un hexagone parmi les hexagones possédés dans ce secteur
+        List<Hex> availableHexes = ownedHexesInSectors.get(selectedSector);
         Hex chosenHex = availableHexes.get(new Random().nextInt(availableHexes.size()));
         int hexIndex = selectedSector.getSection().indexOf(chosenHex);
-        
-        // Effectuer l'expansion sur l'hexagone choisi
+
+        // Ajouter les vaisseaux au hexagone choisi
         System.out.println(getName() + " (bot) is expanding in sector " + selectedSector.getSectorId() + " at hex " + hexIndex);
         selectedSector.expand(hexIndex, shipNumber);
         System.out.println("Bot placed " + shipNumber + " ships on hex " + hexIndex + " in sector " + selectedSector.getSectorId());
