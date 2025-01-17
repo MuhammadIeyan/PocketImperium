7160b3aa4983694f78f3a98e3162d7f14a8dfca4
Muhammad Ieyan <muhammadieyan@gmail.com>
2025-01-17 23:26:34 +0100

    Fixed bot's explore command

--- src/PocketImperium/Game.java
+++ src/PocketImperium/Game.java
@@ -445,7 +445,6 @@ public class Game implements Serializable{
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
